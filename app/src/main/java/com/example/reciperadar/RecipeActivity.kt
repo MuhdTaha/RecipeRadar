54eba6352f448865d5ced6e80b8515c60ae8cf32
MuhdTaha <124110272+MuhdTaha@users.noreply.github.com>
2024-09-19 23:26:13 -0500

    started recipe activity view

---
+++ app/src/main/java/com/example/reciperadar/RecipeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.reciperadar
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.reciperadar.databinding.ActivityRecipeBinding
+
+class RecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)  
+    }
+}
